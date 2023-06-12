6e53aeba9e35196924608032638e8b28dbb32654
normstraker <88446816+normstraker@users.noreply.github.com>
2023-06-12 00:27:40 -0500

    changes

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ const selectPlanButtons = document.querySelectorAll(".plan button");
 const modalNegative = document.querySelector(".modal__action--negative");
 const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
+const ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop)
 
@@ -33,4 +34,14 @@ function closeModal() {
 toggleButton.addEventListener("click", function () {
   mobileNav.classList.add("open");
   backdrop.classList.add("open");
+});
+
+ctaButton.addEventListener("animationstart", function (event) {
+  console.log("Animation started", event);
+});
+ctaButton.addEventListener("animationend", function (event) {
+  console.log("Animation ended", event);
+});
+ctaButton.addEventListener("animationiteration", function (event) {
+  console.log("Animation iteration", event);
 });
